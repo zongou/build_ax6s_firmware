8be750391ffd9a49efa320d2b60611f8f3d77a3c
zongou <zongou@outlook.com>
2022-04-18 00:29:40 +0800

    enable wlan, modify ssid

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,3 +22,8 @@ sed -i "s/OpenWrt /zongou build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" packa
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
+# enable wlan
+sed -i 's/wireless.radio${devidx}.disabled=1/wireless.radio${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# modify wlan name
+sed -i 's/wireless.default_radio${devidx}.ssid=OpenWrt/wireless.default_radio${devidx}.ssid=Redmi_6256/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
